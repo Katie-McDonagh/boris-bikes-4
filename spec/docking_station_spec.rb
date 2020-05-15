edd0dcddb89b10f58126960801731eb7dc9cb8f3
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-15 12:03:20 +0100

    add wrapped collection in DockingStation class, refarctored tests to remove tests no longer needed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,24 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
+
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
-      # we want to release the bike we docked
       expect(subject.release_bike).to eq bike
     end
   end
 
   describe '#release_bike' do
   it 'raises an error when there are no bikes available' do
-    # Let's not dock a bike first:
-    # remember subject == DockingStation.new
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
 end
 
   it {is_expected.to respond_to(:dock).with(1).argument }
 
-
-    it { is_expected.to respond_to(:bike) }
-
-    it 'docks a bike' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
-    end
+  it { is_expected.to respond_to(:bike) }
 
     describe '#dock' do
       it 'raises an error when full' do
@@ -35,15 +27,18 @@ end
       end
     end
 
-   it 'docks a bike' do
-     bike = Bike.new
-     expect(subject.dock(bike)).to eq bike
-   end
-
-   it 'contains a bike' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
-      end
-
 end
+# it 'docks a bike' do
+#   bike = Bike.new
+#   expect(subject.dock(bike)).to eq bike
+# end
+# it 'docks a bike' do
+#   bike = Bike.new
+#   expect(subject.dock(bike)).to eq bike
+# end
+
+# it 'contains a bike' do
+#      bike = Bike.new
+#      subject.dock(bike)
+#      expect(subject.bike).to eq bike
+#    end
