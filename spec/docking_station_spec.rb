91654d86d4a47d3201e4bdcd46101e6a34d69e0f
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-15 09:46:14 +0100

    add failing rspec test rspec now expects an exception

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,20 +2,15 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
-    it {is_expected.to respond_to(:release_bike)}
+    # it {is_expected.to respond_to(:release_bike)}
 
-    
+
     describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
     end
-  end
-    # it 'release_bike' do
-    #     bike = subject.release_bike
-    #     expect(bike.working?).to eq true
-    # end
+
 
   it {is_expected.to respond_to(:dock).with(1).argument }
 
