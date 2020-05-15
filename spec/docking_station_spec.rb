4cf9ccfc8ffcbc77f9c0f91d7e06f47d7ce858e4
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-15 10:20:28 +0100

    rspec pass by adding error catching line

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,22 @@
-
 require './lib/docking_station.rb'
 
 describe DockingStation do
-    # it {is_expected.to respond_to(:release_bike)}
-
-
-    describe '#release_bike' do
-      it 'raises an error when there are no bikes available' do
-        expect { subject.release_bike }.to raise_error 'No bikes available'
-      end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
     end
+  end
 
+  describe '#release_bike' do
+  it 'raises an error when there are no bikes available' do
+    # Let's not dock a bike first:
+    # remember subject == DockingStation.new
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+end
 
   it {is_expected.to respond_to(:dock).with(1).argument }
 
